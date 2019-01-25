d9cb08bc257d6f5ef7389bee5aea3bca4a9d05e8
Eric Valera Miller <eric@elericuz.com>
2019-01-24 23:19:19 -0500

    adding kardex to patients

--- public/components/patients/kardexcontroller.js
+++ public/components/patients/kardexcontroller.js
@@ -1,13 +1,55 @@
 'use strict';
 
 angular.module('patients')
-    .controller('KardexController', function ($scope) {
-        $scope.greeting = "Kardex World";
+    .controller('KardexController', ["$scope", "$http", "$routeParams", function ($scope, $http, $routeParams) {
+        $scope.patientId = $routeParams.id;
 
         $scope.today = function() {
             $scope.dt = new Date();
+            $scope.today = new Date();
         };
         $scope.today();
 
+        function getKardex() {
+            $http({
+                method: 'GET',
+                url: BACKEND + '/kardex/' + $scope.patientId,
+                params: { currentDate: $scope.dt }
+            }).then(function successCallback(response) {
+                $scope.kardexList = response.data;
+            });
+        }
 
-    });
+        function getPatientProfile() {
+            $http({
+                method: 'GET',
+                url: BACKEND + '/patients/profile/' + $scope.patientId
+            }).then(function successCallback(response) {
+                $scope.patientName = response.data[0].xpaciente;
+                $scope.patientId = response.data[0].cpac;
+                getKardex();
+            });
+        }
+        getPatientProfile();
+
+        $scope.dateChange = function() {
+            console.log($scope.dt);
+            getKardex();
+        };
+
+        $scope.saveKardex = function() {
+            $http({
+                method: 'PUT',
+                url: BACKEND + '/kardex/add',
+                data: {
+                    patientId: this.patientId,
+                    currentDate: this.dt,
+                    medicine: this.medicineRecord,
+                    idVia: this.viaIdRecord,
+                    dosis: this.dosisRecord
+                }
+            }).then(function successCallback(response) {
+                getKardex();
+            });
+        };
+    }]);
